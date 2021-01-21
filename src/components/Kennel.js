6ed01ed0c67a635538f541cd950d35673040bb75
MegSemrad <mlsemrad753@gmail.com>
2021-01-21 10:40:21 -0600

    fixed nav bar

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -5,14 +5,17 @@
 
 import React from "react"
 // Import react in every component 
-import { AnimalCard } from "./animal/AnimalCard.js"
-import { LocationCard } from "./location/LocationCard.js"
-import { CustomerCard } from "./customer/CustomerCard.js"
-import { EmployeeCard } from "./employee/EmployeeCard.js"
-import "./animal/Animal.css"
-import "./location/Location.css"
-import "./customer/Customer.css"
-import "./employee/Employee.css"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+// import { AnimalCard } from "./animal/AnimalCard.js"
+// import { LocationCard } from "./location/LocationCard.js"
+// import { CustomerCard } from "./customer/CustomerCard.js"
+// import { EmployeeCard } from "./employee/EmployeeCard.js"
+import "./Kennel.css"
+// import "./animal/Animal.css"
+// import "./location/Location.css"
+// import "./customer/Customer.css"
+// import "./employee/Employee.css"
 
 // Note the () below instead of {} in the function 
 export const Kennel = () => (
@@ -20,13 +23,8 @@ export const Kennel = () => (
        item as return can only return one thing 
     */ 
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
+        <NavBar />
+        <ApplicationViews />
 
         {/* 
         - cannot use "class" but rather must use "className" becuase
@@ -36,7 +34,7 @@ export const Kennel = () => (
         - "AnimalCard" is invoking AnimalCard function -- of course after "Kennel"
           function is invoked whereever 
         */}
-        <h2>Animals</h2>
+        {/* <h2>Animals</h2>
         <article className="animals">
             <AnimalCard />
             <AnimalCard />
@@ -62,6 +60,6 @@ export const Kennel = () => (
             <EmployeeCard />
             <EmployeeCard />
             <EmployeeCard />
-        </article>
+        </article> */}
     </>
 )
