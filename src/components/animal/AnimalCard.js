2ab36dcabd406a557904fc43ce192ba1f75fa8bd
MegSemrad <mlsemrad753@gmail.com>
2021-01-22 13:25:57 -0600

    added owner and location to animals

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -10,5 +10,7 @@ export const AnimalCard = ({animal}) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">Breed: {animal.breed} </div>
+        <div className="animal__owner">Owner: {animal.customer.name}</div>
+        <div className="animal__location">Location: {animal.location.name}</div>
     </section>
 );
