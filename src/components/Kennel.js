afcaeaa147d8b71d3a79ed0b43b0c7d2c6be61c7
MegSemrad <mlsemrad753@gmail.com>
2021-01-28 09:41:02 -0600

    created login affordance for users

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -14,18 +14,38 @@
     - Import react in every component
 */
 
- 
+
 
 import React from "react";
-import { NavBar } from "./nav/NavBar";
+import { Route, Redirect } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 import "./Kennel.css";
 
- 
-
 export const Kennel = () => (
-    <>
-        <NavBar />             {/* Presentation Component */}
-        <ApplicationViews />   {/* Controller Component */}
-    </> 
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kennel_customer")) {
+          return (
+            <>
+              <NavBar />            {/* Presentation Component */}
+              <ApplicationViews />  {/* Controller Component */}
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
 );
